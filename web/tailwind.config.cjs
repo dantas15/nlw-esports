0bfa351c017f48e419a3a0eff36c1ecc93962ff2
Gustavo Galote <gusgalot3g@gmail.com>
2022-09-13 23:51:26 -0300

    feat(web): landing page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       backgroundImage: {
         galaxy: "url(/background-galaxy.png)",
+        "galaxy-gradient":
+          "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%);",
+        "game-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);",
       },
     },
   },
