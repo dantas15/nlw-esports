6ff3729b15e89f9fa347fda10eac64d5b0d37861
Gustavo Galote <gusgalot3g@gmail.com>
2022-09-14 00:58:25 -0300

    feat(mobile): Heading component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,15 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from "./styles";
 
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar" />
+    </View>
+  );
 }
