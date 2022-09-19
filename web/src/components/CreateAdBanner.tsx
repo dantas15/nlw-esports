ea14bf28835b7eb7601e1fb21bd2e81c33859b26
Gustavo Galote <gusgalot3g@gmail.com>
2022-09-18 21:37:41 -0300

    feat(web): add Modal component with Radix

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,22 +1,34 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function CreateAdBanner() {
   return (
-    <div className="mt-8 pt-1 bg-galaxy-gradient self-stretch rounded-lg overflow-hidden">
-      <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
-        <div>
-          <strong className="text-2xl text-white font-black block">
-            Não encontrou seu duo?
-          </strong>
-          <span className="text-zinc-400 block">
-            Publique um anúncio para encontrar novos players
-          </span>
+    <Dialog.Root>
+      <div className="mt-8 pt-1 bg-galaxy-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players
+            </span>
+          </div>
+          <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </Dialog.Trigger>
         </div>
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-          <MagnifyingGlassPlus size={24} />
-          Publicar anúncio
-        </button>
       </div>
-    </div>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+        <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+          <Dialog.Title>Publique um anúncio</Dialog.Title>
+          <form></form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
